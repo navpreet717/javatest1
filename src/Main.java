110ab31d81cf5ad755c8e56eec4f7420e6726b82
748674 <748674@123-PC19.student.Tor.Loc>
2019-06-18 17:35:57 -0400

    r3.2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {	
 	public static void main(String[] args) throws InterruptedException {
-		Rabbit rabbit = new Rabbit();
+		Rabbit rabbit = new Rabbit( 500, 1000);
 		rabbit.sayHello();
-		
 		
 		boolean runForever = true;
 
