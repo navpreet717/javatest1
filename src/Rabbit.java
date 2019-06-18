52bd5afe296c40bc91c822af5425c33e48044a1d
748674 <748674@123-PC19.student.Tor.Loc>
2019-06-18 15:34:56 -0400

    Problem3

---
+++ src/Rabbit.java
@@ -0,0 +1,40 @@
+
+public class Rabbit {
+	
+	// -----------
+	// PROPERTIES
+	// -----------
+	private int xPosition;
+	private int yPosition;
+	
+	
+	// -----------
+	// CONSTRUCTOR 
+	// ------------
+	public Rabbit() {
+
+	}
+	
+	// -----------
+	// METHODS 
+	// ------------
+	public void printCurrentPosition() {
+		System.out.println("The current position of the rabbit is: ");
+	}
+
+	public void sayHello() {
+		System.out.println("Hello! I am a rabbit!");
+	}
+	
+	// ----------------
+	// ACCESSOR METHODS
+	// ----------------
+	
+	// Put all your accessor methods in this section.
+	
+	
+}
+
+
+
+
