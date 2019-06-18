778ad450048e87595cfccb5d1a907b9affba5f7e
748674 <748674@123-PC19.student.Tor.Loc>
2019-06-18 16:42:05 -0400

    calculate i==j

--- src/SubstringForkids.java
+++ src/SubstringForkids.java
@@ -3,12 +3,27 @@
 public class SubstringForkids {
 
 	public static void main(String[] args) {
-		System.out.println("SUBSTRING FOR KIDS");
+		System.out.println("substring for the kids");
+		
+		int t=9;
+		int k=8;
+		String s="navpreet";
+	String commit1=	substringForKids(t,k,s);
+	System.out.println(commit1);
+		
+	
+	}
 
+	
+	public static String substringForKids(int i, int j, String sentence) {
+		
+String result="";
+	 if(i == j) {
+		return "" + sentence.charAt(i);
+} 
+		return "numbers are not equal";
 	}
 	
-	public String substringForKids() {
-		return "";
-	}
 }
+
 
