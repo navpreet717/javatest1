5e6bf67408c04d950f2f2f65b51fcaff9cdd0805
748674 <748674@123-PC19.student.Tor.Loc>
2019-06-18 15:30:55 -0400

    [program1 starter

---
+++ src/BuyingPhones.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class BuyingPhones {
+	
+
+		public static void main(String[] args) {
+			System.out.println("Enter the regular price of a phone: ");
+			Scanner s = new Scanner(System.in);
+			double price = s.nextInt();
+			
+
+		}
+
+	}
+
+
